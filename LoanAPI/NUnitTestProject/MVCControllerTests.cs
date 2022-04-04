1e08ef663b7a1702d2c8099d4a000fd631cfe89f
nikpapag <nikolaos.papageorgiou@harness.io>
2022-04-04 15:32:59 +0100

    added Nunit

---
+++ LoanAPI/NUnitTestProject/MVCControllerTests.cs
@@ -0,0 +1,129 @@
+﻿using NUnit.Framework;
+using System;
+using Loan.Data.Models;
+using System.Threading;
+using LoanAPI.Controllers;
+using Loan.Data.Interfaces;
+using Moq;
+
+
+namespace NUnitTestProject
+{
+    public class MVCControllerTests
+    {
+        [Test]
+        public void LoanReturnsCorrectIdTest()
+        {
+            var mockLoanRepo = new Mock<ILoanRepository>();
+
+            var LoanId = 2;
+
+            mockLoanRepo.Setup(x => x.GetLoan(It.IsAny<int>())).Returns(new LoanApplication() { Id = 2, Type = "mortgage", Amount = 100000, isVariable = true, Rate = 2.5, RepaymentYears = 25 });
+            var controller = new LoansController(mockLoanRepo.Object);
+
+            var response = controller.GetLoan(LoanId);
+
+
+            Assert.AreEqual(LoanId, response.Value.Id);
+
+
+        }
+
+        [Test]
+        public void LoanReturnsCorrectTypeTest()
+        {
+            var mockLoanRepo = new Mock<ILoanRepository>();
+            var LoanId = 2;
+            var type = "mortgage";
+
+            mockLoanRepo.Setup(x => x.GetLoan(It.IsAny<int>())).Returns(new LoanApplication() { Id = 2, Type = "mortgage", Amount = 100000, isVariable = true, Rate = 2.5, RepaymentYears = 25 });
+            var controller = new LoansController(mockLoanRepo.Object);
+
+            var response = controller.GetLoan(LoanId);
+
+
+            Assert.AreEqual(type, response.Value.Type);
+
+
+        }
+        [Test]
+        public void LoanReturnsCorrectAmountTest()
+        {
+            var mockLoanRepo = new Mock<ILoanRepository>();
+            var LoanId = 2;
+            var amount = 100000;
+
+            mockLoanRepo.Setup(x => x.GetLoan(It.IsAny<int>())).Returns(new LoanApplication() { Id = 2, Type = "mortgage", Amount = 100000, isVariable = true, Rate = 2.5, RepaymentYears = 25 });
+            var controller = new LoansController(mockLoanRepo.Object);
+
+            var response = controller.GetLoan(LoanId);
+
+
+            Assert.AreEqual(amount, response.Value.Amount);
+
+
+        }
+
+
+
+        [Test]
+        public void LoanReturnsCorrectVariableTest()
+        {
+            var mockLoanRepo = new Mock<ILoanRepository>();
+            var LoanId = 2;
+            var isVariable = true;
+
+            mockLoanRepo.Setup(x => x.GetLoan(It.IsAny<int>())).Returns(new LoanApplication() { Id = 2, Type = "mortgage", Amount = 100000, isVariable = true, Rate = 2.5, RepaymentYears = 25 });
+            var controller = new LoansController(mockLoanRepo.Object);
+
+            var response = controller.GetLoan(LoanId);
+
+
+            Assert.AreEqual(isVariable, response.Value.isVariable);
+
+
+        }
+
+
+
+
+
+        [Test]
+        public void LoanReturnsCorrectRepaymentYearsTest()
+        {
+            var mockLoanRepo = new Mock<ILoanRepository>();
+            var LoanId = 2;
+            var RepaymentYears = 25;
+
+            mockLoanRepo.Setup(x => x.GetLoan(It.IsAny<int>())).Returns(new LoanApplication() { Id = 2, Type = "mortgage", Amount = 100000, isVariable = true, Rate = 2.5, RepaymentYears = 25 });
+            var controller = new LoansController(mockLoanRepo.Object);
+
+            var response = controller.GetLoan(LoanId);
+
+
+            Assert.AreEqual(RepaymentYears, response.Value.RepaymentYears);
+
+
+        }
+
+
+
+        [Test]
+        public void LoanReturnsCorrectRatesTest()
+        {
+            var mockLoanRepo = new Mock<ILoanRepository>();
+            var LoanId = 2;
+            var Rate = 2.5;
+
+            mockLoanRepo.Setup(x => x.GetLoan(It.IsAny<int>())).Returns(new LoanApplication() { Id = 2, Type = "mortgage", Amount = 100000, isVariable = true, Rate = 2.5, RepaymentYears = 25 });
+            var controller = new LoansController(mockLoanRepo.Object);
+
+            var response = controller.GetLoan(LoanId);
+
+
+            Assert.AreEqual(Rate, response.Value.Rate);
+
+
+        }
+    }
+}
---
+++ LoanAPI/NUnitTestProject/POJOTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Loan.Data.Models;
+using System;
+using System.Threading;
+
+
+namespace NUnitTestProject
+{
+    public class POJOTests
+    {
+        private LoanApplication loan;
+        [SetUp]
+        public void Setup()
+        {
+            loan = new LoanApplication();
+        }
+
+        [Test]
+        public void LoanRateTest()
+        {
+
+            loan.Rate = 2.5;
+            Thread.Sleep(10000);
+            Assert.AreEqual(2.5, loan.Rate);
+
+        }
+        [Test]
+        public void LoanTypeTest()
+        {
+
+            loan.Type = "mortgage";
+            Thread.Sleep(10000);
+            Assert.AreEqual("mortgage", loan.Type);
+        }
+        [Test]
+        public void LoanDuration()
+        {
+
+            loan.RepaymentYears = 10;
+            Thread.Sleep(10000);
+            Assert.AreEqual(10, loan.RepaymentYears);
+        }
+    }
+}
